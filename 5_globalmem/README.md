3f18bc33a47c9dc26a55a9ec02c55ba40f8d9e80
lidonghang-02 <2426971102@qq.com>
2024-05-04 15:12:09 +0800

    update

---
+++ 5_globalmem/README.md
@@ -0,0 +1,2 @@
+> 创建10个同样globalmem设备，都在同一个globalmem类中，可以在`/sys/class/globalmem/`下看到
+> 每个设备都使用互斥锁mutex
---
+++ 5_globalmem/beep.h
@@ -0,0 +1,8 @@
+#ifndef _PWM_H
+#define _PWM_H
+
+#define DEV_FIFO_TYPE 'k'
+
+#define DEV_FIFO_CLEAN _IO(DEV_FIFO_TYPE, 0)
+
+#endif
